328e53d3471c680316aa975a2f1ec37b0ffc0fba
aRTIKa-afk <artiiv240606@mail.ru>
2024-12-12 22:04:54 +0300

    rename oneletter variable

--- main.cpp
+++ main.cpp
@@ -49,14 +49,14 @@ int Find(Node* root, int key) {
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
-  int in_n;
-  std::cin >> in_n;
-  std::vector<std::pair<std::string, std::string>> login_to_password(in_n);
-  std::vector<std::pair<std::string, std::string>> password_to_login(in_n);
-  std::vector<std::pair<int, int>> login_to_password_like_ind(in_n);
-  std::vector<std::pair<int, int>> password_to_login_like_ind(in_n);
+  int in_num;
+  std::cin >> in_num;
+  std::vector<std::pair<std::string, std::string>> login_to_password(in_num);
+  std::vector<std::pair<std::string, std::string>> password_to_login(in_num);
+  std::vector<std::pair<int, int>> login_to_password_like_ind(in_num);
+  std::vector<std::pair<int, int>> password_to_login_like_ind(in_num);
   std::vector<std::string> logins_and_passwords;
-  for (int i = 0; i < in_n; i++) {
+  for (int i = 0; i < in_num; i++) {
     std::cin >> login_to_password[i].first;
     std::cin >> login_to_password[i].second;
     password_to_login[i].first = login_to_password[i].second;
@@ -68,7 +68,7 @@ int main() {
   logins_and_passwords.resize(
       std::unique(logins_and_passwords.begin(), logins_and_passwords.end()) -
       logins_and_passwords.begin());
-  for (int i = 0; i < in_n; i++) {
+  for (int i = 0; i < in_num; i++) {
     login_to_password_like_ind[i].first =
         std::lower_bound(logins_and_passwords.begin(),
                          logins_and_passwords.end(),
@@ -95,12 +95,12 @@ int main() {
   std::sort(password_to_login_like_ind.begin(),
             password_to_login_like_ind.end());
   Node* root_login_to_password =
-      Create(login_to_password_like_ind, 0, in_n - 1);
+      Create(login_to_password_like_ind, 0, in_num - 1);
   Node* root_password_to_login =
-      Create(password_to_login_like_ind, 0, in_n - 1);
-  int in_q;
-  std::cin >> in_q;
-  for (int i = 0; i < in_q; i++) {
+      Create(password_to_login_like_ind, 0, in_num - 1);
+  int count_of_query;
+  std::cin >> count_of_query;
+  for (int i = 0; i < count_of_query; i++) {
     std::string query;
     std::cin >> query;
     int index_query = std::lower_bound(logins_and_passwords.begin(),
